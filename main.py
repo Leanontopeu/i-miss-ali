1fab5368be969e009dbd9cbe7b800d082910648d
Leanontopeu <140197779+Leanontopeu@users.noreply.github.com>
2026-02-09 19:57:26 +0000

    Remove Discord bot implementation

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-import os
-import discord
 
-TOKEN = os.environ.get("MTQ3MDQ1NTMwMjMxMzg2OTQ4NQ.Gny9_")
-
-intents = discord.Intents.default()
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-  print(f"Logged in as {client.user}")
-  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.Playing name="I miss my ali :c"
-                                                                                           )
-                                                        )
-  client.run(TOKEN)
