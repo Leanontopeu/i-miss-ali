87311d18ae6070da3a4eb41860f7514188a28dea
Leanontopeu <140197779+Leanontopeu@users.noreply.github.com>
2026-02-09 19:59:03 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import os
+import discord
 
+TOKEN = os.environ.get("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+    await client.change_presence(
+        activity=discord.Activity(
+            type=discord.ActivityType.playing,
+            name="i miss ali :c"
+        )
+    )
+
+client.run(TOKEN)
